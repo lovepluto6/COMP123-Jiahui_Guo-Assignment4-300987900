bb33c53bc2fed061a21ec280f15b7641afcb2ad3
Guo Jiahui <luanrenfeng@gmail.com>
2019-07-25 10:10:35 +0800

    添加项目文件。

---
+++ COMP123-Jiahui_Guo-Assignment4-300987900/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_Jiahui_Guo_Assignment4_300987900
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
